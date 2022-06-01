644ed6bf49e5d1bdc56b2b819843b613193b8f0c
TarenShaw <tshaaaaw@gmail.com>
2022-06-01 23:57:40 +0200

    Fct visual reorder

--- Diversity_Analystics.R
+++ Diversity_Analystics.R
@@ -6,16 +6,35 @@ skim(df)
 
 glimpse(df)
 
-table(df$gender,df$marital_status)
+table(df$gender, df$marital_status)
 
 table(df$gender)
 
-df %>% count(gender) %>% mutate(n_tot = sum(n), p_tot = n/n_tot)
+df %>%
+  group_by(gender, job_role) %>%
+  summarise(n = n()) %>%
+  tibble() %>%
+  left_join(., df %>% count(job_role), by = "job_role") %>%
+  mutate(prop = n.x / n.y)
 
-ggplot(data=df) +      
-  geom_bar(aes(x= job_role, fill=gender), , position = "fill") +
+male_fct <- df %>%
+  group_by(gender, job_role) %>%
+  summarise(n = n()) %>%
+  tibble() %>%
+  left_join(., df %>% count(job_role), by = "job_role") %>%
+  mutate(prop = n.x / n.y) %>%
+  filter(gender == "Male") %>%
+  select(job_role, prop) %>%
+  arrange(desc(prop)) %>% print()
+
+df$job_role <- factor(df$job_role)
+
+df <- left_join(df, male_fct, by = "job_role")
+
+df <- df %>% mutate(job_role = fct_reorder(df$job_role, df$prop, max))
+
+ggplot(data = df) +
+  geom_bar(aes(x = job_role, fill = gender), position = "fill") +
   coord_flip() +
   theme_bw() +
   scale_fill_brewer()
-
-
